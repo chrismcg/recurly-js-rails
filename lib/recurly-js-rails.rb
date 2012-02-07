7f6dab1f0e941f490567969cb92bee949bd66fc8
Chris McGrath <chris@octopod.info>
2012-02-07 16:44:20 +0200

    bundle gem recurly-js-rails skeleton

---
+++ lib/recurly-js-rails.rb
@@ -0,0 +1,9 @@
+require "recurly-js-rails/version"
+
+module Recurly
+  module Js
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/recurly-js-rails/version.rb
@@ -0,0 +1,7 @@
+module Recurly
+  module Js
+    module Rails
+      VERSION = "0.0.1"
+    end
+  end
+end
